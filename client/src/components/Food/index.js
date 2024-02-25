f4de886aa4f213f0ac030ab3f79d576b9a0c22b9
Saumya Dutta <saumyaxdutta@gmail.com>
2024-02-25 00:43:05 -0500

    Finished food table, food table clear, and water table stories

--- client/src/components/Food/index.js
+++ client/src/components/Food/index.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import FoodTable from './FoodPage';
+import WaterInput from './WaterInput';
+import Stack from '@mui/material/Stack';
 
 export default function Food(){
     return (
         <div>
             <FoodTable/>
+            <WaterInput/>
         </div>
     )
 }
